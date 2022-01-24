aa8012156a15554ee214d458eac7f3a3edc02c3d
Rodrigo Albano <rodrigoalbanor@gmail.com>
2022-01-24 13:55:43 -0300

    first commit

---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ProductItem from '@components/ProductItem';
+import '@styles/ProductList.scss';
+import useGetProduct from '@hooks/useGetProducts';
+
+const API = 'http://api.escuelajs.co/api/v1/products';
+
+const ProductList = () => {
+	const products = useGetProduct(API);
+
+	return (
+		<section className="main-container">
+			<div className="ProductList">
+				{products.map(product => (
+					<ProductItem product={product} key={product.id}/>
+				))}
+				
+			</div>
+		</section>
+	);
+}
+
+export default ProductList;
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            Not encontrado, error 404.
+        </div>
+    );
+}
+
+
+export default NotFound;
